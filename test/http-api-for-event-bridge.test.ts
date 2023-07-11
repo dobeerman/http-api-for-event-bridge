7d8a4cc3655d216447e7b0067ee1f38a96584de3
Alexander <alexander.smirnoff@sportec-solutions.com>
2023-07-11 12:50:15 +0200

    Initial commit

---
+++ test/http-api-for-event-bridge.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HttpApiForEventBridge from '../lib/http-api-for-event-bridge-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/http-api-for-event-bridge-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HttpApiForEventBridge.HttpApiForEventBridgeStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
